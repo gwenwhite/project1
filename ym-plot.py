731c5721b6b877cba50d3c700c55fb5c3cc4b1b8
gwenwhite <gwenwhite@u.boisestate.edu>
2021-03-12 17:19:29 -0700

    Added a title to my plot.

---
+++ ym-plot.py
@@ -0,0 +1,9 @@
+import numpy 
+import matplotlib.pyplot as plt
+from numpy import diff
+import os
+a= numpy.loadtxt('samples/1.raw', skiprows=1, delimiter=",")
+
+plt.plot(a[:,0], a[:,1])
+plt.title("Youngs Modulus of Samples")
+plt.show()
