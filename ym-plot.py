ad3cb9ebf55411714cb8a703a7e94f3ad61ad4a4
gwenwhite <gwenwhite@u.boisestate.edu>
2021-03-12 17:33:29 -0700

    Added a title to my graph.

--- ym-plot.py
+++ ym-plot.py
@@ -5,5 +5,5 @@ import os
 a= numpy.loadtxt('samples/1.raw', skiprows=1, delimiter=",")
 
 plt.plot(a[:,0], a[:,1])
-plt.title("Youngs Modulus of Samples")
+plt.title("Youngs Modulus of Sample")
 plt.show()
